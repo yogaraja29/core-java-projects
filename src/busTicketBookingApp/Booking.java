19fcb0535e471d574e00e64b0ee2757f74290751
yogaraja <s.y.raja97@gmail.com>
2024-10-27 16:17:15 +0530

    Added booking functionality

--- src/busTicketBookingApp/Booking.java
+++ src/busTicketBookingApp/Booking.java
@@ -1,47 +1,37 @@
 package busTicketBookingApp;
 
-import java.util.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Booking {
-
+	private String passengerName;
 	private int busNumber;
-	private int seatCount;
 	private Date journeyDate;
 
-	Booking() {
-		Scanner scan = new Scanner(System.in);
-
-		System.out.println("Enter Bus Number: ");
-		this.busNumber = scan.nextInt();
-		System.out.println("How many seats do you want? ");
-		this.seatCount = scan.nextInt();
-		System.out.println("Enter journey date(dd-mm-yyyy): ");
-		String dateInput = scan.next();
-
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-		try {
-			this.journeyDate = dateFormat.parse(dateInput);
-		} catch (ParseException e) {
-
-			e.printStackTrace();
-		}
-//		System.out.println(this.busNumber + " " + this.seatCount + " " + this.journeyDate);
-		scan.close();
+	Booking(String passengerName, int busNumber, Date journeyDate) {
+		this.passengerName = passengerName;
+		this.busNumber = busNumber;
+		this.journeyDate = journeyDate;
 	}
 
 	public boolean isSeatAvailable(ArrayList<Bus> buses, ArrayList<Booking> bookings) {
-		int availableSeats = 0;
+		int availableSeats = 0, bookedSeats = 0;
 		for (Bus bus : buses) {
 			if (this.busNumber == bus.getBusNumber()) {
 				availableSeats = bus.getCapacity();
 			}
-			if (this.seatCount < availableSeats) {
-				return true;
+		}
+		for (Booking booking : bookings) {
+			if (booking.busNumber == this.busNumber && booking.journeyDate.equals(this.journeyDate)) {
+				bookedSeats++;
 			}
 		}
-		return false;
+
+		return bookedSeats < availableSeats;
+	}
+
+	public void bookingInfo() {
+		System.out.println(this.passengerName + " " + this.busNumber + " " + this.journeyDate);
 	}
 
 }
--- src/busTicketBookingApp/BusTicketBooking.java
+++ src/busTicketBookingApp/BusTicketBooking.java
@@ -1,5 +1,7 @@
 package busTicketBookingApp;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class BusTicketBooking {
@@ -9,18 +11,42 @@ public class BusTicketBooking {
 		Scanner scan = new Scanner(System.in);
 
 		ArrayList<Bus> buses = new ArrayList<Bus>();
+		buses.add(new Bus(1, true, 1));
+		buses.add(new Bus(2, false, 2));
 		ArrayList<Booking> bookings = new ArrayList<Booking>();
-		buses.add(new Bus(1, true, 2));
-		buses.add(new Bus(2, false, 3));
-
-		int choice;
-
-		do {
-			System.out.println("---------------\n1.New Booking\n2.Bus Info\n3.Exit\n-----------------");
+		
+		int choice = 0;
+		while (true) {
+			System.out.println("---------------\n1.New Booking\n2.Bus Info\n3.Print All Bookings\n4.Exit\n--------------");
 			choice = scan.nextInt();
 			switch (choice) {
 			case 1:
-				Booking booking = new Booking();
+				String passengerName;
+				int busNumber;
+				Date journeyDate = null;
+
+				System.out.println("Enter Passenger Name: ");
+				passengerName = scan.next();
+				System.out.println("Enter Bus Number: ");
+				busNumber = scan.nextInt();
+				System.out.println("Enter journey date(dd-mm-yyyy): ");
+				String dateInput = scan.next();
+				// convert string to date object
+				SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+				try {
+					journeyDate = dateFormat.parse(dateInput);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+
+				Booking booking = new Booking(passengerName, busNumber, journeyDate);
+
+				if (booking.isSeatAvailable(buses, bookings)) {
+					bookings.add(booking); // append into bookings list
+					System.out.println(passengerName + " has booked ticket successfully!");
+				} else {
+					System.out.println("Bus is full. Try another bus or date!");
+				}
 				break;
 			case 2:
 				for (Bus bus : buses) {
@@ -28,13 +54,18 @@ public class BusTicketBooking {
 				}
 				break;
 			case 3:
+				for (Booking b : bookings) {
+					b.bookingInfo();
+				}
+				break;
+			case 4:
 			default:
-				System.out.println("-----Thank you!!!-----");
+				System.out.println("Thank you!!!");
+				scan.close();
 				System.exit(0);
 			}
-		} while (choice >= 1 && choice <= 2);
 
-		scan.close();
+		}
 
 	}
 
